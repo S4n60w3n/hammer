222a2633227b70cf91e81684d8a9fc1b32f1ab31
Bogdan Sikora <sikora.bogdan@gmail.com>
2018-11-18 18:56:22 +0100

    getMean added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,8 +7,20 @@ export const getStandardDeviation = (args: number[]): number => {
   if (!args) { return -1; }
   if (args.length <= 1) { return 0; }
   const sum = args.reduce((value, item) => value + item, 0);
-  const mean = sum / args.length;
+  const mean = getMean(args);
   const meanDiff = args.map((item) => Math.pow(item - mean, 2)).reduce((value, item) => value + item, 0);
 
   return Math.sqrt(meanDiff / (args.length - 1));
+};
+
+/**
+ * Get mean from an array of numbers
+ * @param args array of numbers to get mean from
+ * @returns mean or -1 for error
+ */
+export const getMean = (args: number[]): number => {
+  if (!args) { return -1; }
+  if (args.length === 0) { return 0; }
+
+  return args.reduce((value, item) => value + item, 0) / args.length;
 };
--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,27 +1,51 @@
-import { getStandardDeviation } from 'utils';
+import { getStandardDeviation, getMean } from 'utils';
 
-describe('StandardDeviation', () => {
-  it('Empty', () => {
-    expect(getStandardDeviation([])).toBe(0);
+describe('Utils', () => {
+  describe('getStandardDeviation', () => {
+    it('Empty', () => {
+      expect(getStandardDeviation([])).toBe(0);
+    });
+
+    it('null', () => {
+      expect(getStandardDeviation(null)).toBe(-1);
+    });
+
+    it('undefined', () => {
+      expect(getStandardDeviation(undefined)).toBe(-1);
+    });
+
+    it('single number', () => {
+      expect(getStandardDeviation([5])).toBe(0);
+    });
+
+    it('two numbers', () => {
+      expect(getStandardDeviation([5, 6]).toPrecision(3)).toBe('0.707');
+    });
+
+    it('multiple numbers', () => {
+      expect(getStandardDeviation([727.7, 1086.5, 1091, 1361.3, 1490.5, 1956.1])).toBe(420.96248961952256);
+    });
   });
 
-  it('null', () => {
-    expect(getStandardDeviation(null)).toBe(-1);
-  });
+  describe('getMean', () => {
+    it('Empty', () => {
+      expect(getMean([])).toBe(0);
+    });
 
-  it('undefined', () => {
-    expect(getStandardDeviation(undefined)).toBe(-1);
-  });
+    it('null', () => {
+      expect(getMean(null)).toBe(-1);
+    });
 
-  it('single number', () => {
-    expect(getStandardDeviation([5])).toBe(0);
-  });
+    it('undefined', () => {
+      expect(getMean(undefined)).toBe(-1);
+    });
 
-  it('two numbers', () => {
-    expect(getStandardDeviation([5, 6]).toPrecision(3)).toBe('0.707');
-  });
+    it('single number', () => {
+      expect(getMean([5])).toBe(5);
+    });
 
-  it('multiple numbers', () => {
-    expect(getStandardDeviation([727.7, 1086.5, 1091, 1361.3, 1490.5, 1956.1])).toBe(420.96248961952256);
+    it('two numbers', () => {
+      expect(getMean([5, 6])).toBe(5.5);
+    });
   });
 });
