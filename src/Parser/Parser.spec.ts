8c9ef0de37617763dbbfc603f4acd42f091a91c8
Bogdan Sikora <sikora.bogdan@gmail.com>
2018-11-19 17:22:35 +0100

    Input result reworked

--- src/Parser/Parser.spec.ts
+++ src/Parser/Parser.spec.ts
@@ -1,22 +1,6 @@
 import { parseInput } from 'Parser';
 
 describe('parseInput', () => {
-  it('Empty', () => {
-    expect(parseInput(``)).toBe(null);
-  });
-
-  it('null', () => {
-    expect(parseInput(null)).toBe(null);
-  });
-
-  it('undefined', () => {
-    expect(parseInput(undefined)).toBe(null);
-  });
-
-  it('start without reference', () => {
-    expect(parseInput('Random text')).toBeNull();
-  });
-
   it('only reference', () => {
     const result = parseInput('reference 70.0 45.0');
     expect(result).not.toBeNull();
@@ -34,8 +18,7 @@ thermometer temp-2
 2007-04-05T22:05 69.8
 `);
     expect(result).not.toBeNull();
-    expect(result.readings.length).toBe(1);
-    expect(result.readings[0].name).toBe('temp-2');
+    expect(result.readings['temp-2'].readingType).toBe('thermometer');
   });
 
   it('data, only two', () => {
@@ -54,7 +37,69 @@ thermometer temp-3
 2007-04-05T22:05 69.8
 `);
     expect(result).not.toBeNull();
-    expect(result.readings.length).toBe(2);
-    expect(result.readings[1].name).toBe('temp-3');
+    expect(result.readings['temp-3'].readingType).toBe('thermometer');
+  });
+
+  describe('Error', () => {
+    it('Empty', () => {
+      expect(parseInput(``)).toBe(null);
+    });
+
+    it('null', () => {
+      expect(parseInput(null)).toBe(null);
+    });
+
+    it('undefined', () => {
+      expect(parseInput(undefined)).toBe(null);
+    });
+
+    it('start without reference', () => {
+      expect(parseInput('Random text')).toBeNull();
+    });
+
+    it('Reference without data', () => {
+      expect(parseInput('Reference')).toBeNull();
+    });
+
+    it('Reference without numberic data', () => {
+      expect(parseInput('Reference a2 2')).toBeNull();
+    });
+
+    it('Thermomether without name', () => {
+      const result = parseInput(`reference 70.0 45.0
+thermometer
+2007-04-05T22:01 69.5
+2007-04-05T22:02 70.1
+2007-04-05T22:03 71.3
+2007-04-05T22:04 71.5
+2007-04-05T22:05 69.8
+`);
+      expect(result.readings.unnamed).not.toBeNull();
+      expect(result.readings.unnamed).not.toBeUndefined();
+    });
+
+    it('Thermomether non numeric data', () => {
+      const result = parseInput(`reference 70.0 45.0
+thermometer temp
+2007-04-05T22:01 6a9.5
+`);
+      expect(result.readings.temp.data[0].value).toBeNaN();
+    });
+
+    it('Starts with newLine', () => {
+      const result = parseInput(`
+reference 70.0 45.0
+thermometer temp
+2007-04-05T22:01 69.5
+`);
+      expect(result.readings.temp.data[0].value).toBe(69.5);
+    });
+
+    it('Ends without newLine', () => {
+      const result = parseInput(`reference 70.0 45.0
+thermometer temp
+2007-04-05T22:01 69.5`);
+      expect(result.readings.temp.data[0].value).toBe(69.5);
+    });
   });
 });
--- src/Parser/Parser.ts
+++ src/Parser/Parser.ts
@@ -1,20 +1,27 @@
-export type READING_TYPE = 'thermometer' | 'humidity';
-export type READING = {
-  name: string;
+export type ReadingType = 'thermometer' | 'humidity';
+
+export type Reading = {
   readingType: string;
-  data: string[];
+  data: DataType[];
 };
+
 export type InputResult = {
   refs_therm: number;
   refs_hum: number;
-  readings: READING[];
+  readings: { [name: string]: Reading };
+};
+
+export type DataType = {
+  time: string;
+  value: number;
 };
 
 const THERMOMETER = 'thermometer';
 const HUMIDITY = 'humidity';
+const UNNAMED = 'unnamed';
 
-const READING_REGEX = `(${THERMOMETER}|${HUMIDITY})\\s+(.*)`;
-const REFERENCE_REGEX = `\^reference\\s+(.*)\\s+(.*)`;
+const READING_REGEX = `(${THERMOMETER}|${HUMIDITY})\\s*(.*)`;
+const REFERENCE_REGEX = `\^\\s*reference\\s+(.*)\\s+(.*)`;
 
 /**
  * Parse log
@@ -26,28 +33,39 @@ export const parseInput = (input: string): InputResult => {
   const ret: InputResult = {
     refs_therm: parseInt(refRegexResult[1], 10),
     refs_hum: parseInt(refRegexResult[2], 10),
-    readings: []
+    readings: {}
   };
-  let reading: READING = null;
+  let reading: Reading = null;
+  let name: string = UNNAMED;
   input.match(/[^\r\n]+/g).forEach((line) => {
     const regex = line.match(READING_REGEX);
     if (regex) {
       if (reading) {
-        ret.readings.push(reading);
+        ret.readings[name] = reading;
         reading = null;
+        name = null;
       }
+      name = regex[2] || UNNAMED;
       reading = {
-        name: regex[2],
         readingType: regex[1],
         data: []
       };
     } else if (reading) {
-      reading.data.push(line);
+      reading.data.push(parseData(line));
     }
   });
   if (reading) {
-    ret.readings.push(reading);
+    ret.readings[name] = reading;
   }
 
   return ret;
+};
+
+const parseData = (dataLine: string): DataType => {
+  const [ time, value ] = dataLine.split(' ');
+
+  return {
+    time,
+    value: Number(value)
+  };
 };
